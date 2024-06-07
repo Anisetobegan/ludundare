491e6beec941caa4983f606aebe77474b6d31206
EZEQUIEL\ezequ <ezequiel_morales_1999@hotmail.com>
2024-06-07 19:46:03 -0300

    Enemy Spawn Commit

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemies : MonoBehaviour
+{
+    float speed;
+    float health;
+    float damage;
+    float attackSpeed;
+    float attackRange;
+
+    [SerializeField] NavMeshAgent agent;
+
+    virtual protected void Move()
+    {
+
+    }
+
+    virtual protected void Attack()
+    {
+
+    }
+
+    virtual protected void Die()
+    {
+
+    }
+
+}
